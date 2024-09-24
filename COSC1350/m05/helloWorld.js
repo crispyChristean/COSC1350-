27719c406f8aed2af8b8960c094531d41890d8b2
Christian <christianespinozacel@westernwyoming.edu>
2024-09-23 21:56:24 -0600

    Add files via upload

---
+++ COSC1350/m05/helloWorld.js
@@ -0,0 +1,11 @@
+/* 
+
+    Christian Espinoza
+    File Name: "helloWorld.js"
+    Date: 09/23/2024
+
+*/
+
+let hello = "Hello World";
+
+console.log(hello);
---
+++ COSC1350/m05/practiceVariables.js
@@ -0,0 +1,34 @@
+/* 
+
+Christian Celis
+
+Date:09/23/24
+
+file name: practiceVariable.js
+
+*/
+
+let drT = "Hi Dr. T!"
+console.log(drT);
+
+let floatNumber = 4.5
+console.log(floatNumber);
+
+let weirdZero = 0
+console.log(weirdZero);
+
+let negativeNumber= -10
+console.log(negativeNumber);
+
+let booleanVariable = true
+console.log(booleanVariable);
+
+
+console.log("Hi Dr. T!\nLet's get started!");
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == true);
